ae05e058f3b0e232a12a11420a1e544637cfb188
Michał Stanek <michal.stanek@me.com>
2017-04-22 09:56:53 +0200

    first commit

---
+++ Zadanie 1/project_model/README.md
@@ -0,0 +1,4 @@
+project_model
+=============
+
+A Symfony project created on April 22, 2017, 9:43 am.
---
+++ Zadanie 1/project_model/src/CodersLabBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CodersLabBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('CodersLabBundle:Default:index.html.twig');
+    }
+}
